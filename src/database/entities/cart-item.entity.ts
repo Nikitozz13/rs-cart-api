88eee7e82e19a16c499c85f846a679755869f80a
Nikita Staroseltsev <nikitozz_13@mail.ru>
2023-04-14 19:29:10 +0300

    TASK-8 Update source code in the application to use PostgreSQL instead of memory storage

---
+++ src/database/entities/cart-item.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Cart } from './cart.entity';
+
+@Entity()
+export class CartItem {
+  @ManyToOne(
+    () => Cart,
+    (cart) => cart.items,
+    {
+      nullable: false,
+      primary: true
+    }
+  )
+  @JoinColumn({ name: 'cart_id', referencedColumnName: 'id' })
+  cart: Cart;
+
+  // @OneToOne(() => Product, (product) => product.id)
+  // product: Product;
+  @Column({ type: 'uuid', nullable: false })
+  productId: string;
+
+  @Column({ type: 'integer' })
+  count: string;
+}
---
+++ src/database/entities/cart.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { CartItem } from './cart-item.entity';
+
+@Entity()
+export class Cart {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  // @ManyToOne(() => User, (user) => user.carts)
+  // user: User;
+  @Column({ type: 'uuid', nullable: false })
+  userId: string;
+
+  @Column({ type: 'date', nullable: false })
+  createdAt: string;
+
+  @Column({ type: 'date', nullable: false })
+  updatedAt: string;
+
+  @Column({ type: 'text', nullable: false })
+  status: string; // enum ?
+
+  @OneToMany(() => CartItem, (cartItem) => cartItem.cart)
+  @JoinColumn({ name: 'id', referencedColumnName: 'cart_id' })
+  items: CartItem[];
+}
